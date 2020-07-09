d19428def475ab96ee913632c3342a3c5b2ca60b
Allan Lago <allanlago1@gmail.com>
2020-07-09 05:59:05 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hexagon from "react-hexagon";
+import civs from "./civilizations.json";
+import "./App.css";
 
 function App() {
+  const cells_per_row = 10;
+  const n_rows = 3;
+
+  const parseCivInfo = (civ) => {
+    let civInfo = civ;
+    for (let param of Object.keys(civInfo)) {
+      if (civInfo[param] === "") {
+        civInfo[param] = civs.default[param];
+      }
+    }
+    return civInfo;
+  };
+
+  const hexGrid = Object.keys(civs).map((elem, index) => {
+    if (elem === "default") return <></>;
+
+    let civInfo = parseCivInfo(civs[elem]);
+    return (
+      <Hexagon
+        key={Object.keys(civs)[index + 1]}
+        className="hex"
+        backgroundImage={civInfo.portrait}
+        backgroundScale={1.03}
+        onClick={() => console.log(civInfo)}
+      />
+    );
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {[...Array(n_rows).keys()].map((row_index) => (
+          <div className="row">
+            {[...Array(cells_per_row).keys()].map(
+              (col_index) => hexGrid[cells_per_row * row_index + col_index]
+            )}
+          </div>
+        ))}
       </header>
     </div>
   );
