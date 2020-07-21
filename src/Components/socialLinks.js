807775c98c531c79f342d61bd35c3c6f9e7f96de
Allan Lago <allanlago1@gmail.com>
2020-07-21 17:07:03 -0400

    Updated About me Icons

--- src/Components/socialLinks.js
+++ src/Components/socialLinks.js
@@ -1,30 +1,34 @@
 import React from "react";
-import { TiSocialTwitter, TiSocialLinkedin } from "react-icons/ti";
-import { AiOutlineGlobal, AiOutlineGithub } from "react-icons/ai";
+import {
+  IoIosGlobe,
+  IoLogoGithub,
+  IoLogoTwitter,
+  IoLogoLinkedin,
+} from "react-icons/io";
 
 export const socialLinks = {
   twitter: {
     title: "Twitter",
     subtitle: "@lago_allan",
-    component: <TiSocialTwitter className="clickable-image" />,
+    component: <IoLogoTwitter className="clickable-image" />,
     href: "https://twitter.com/lago_allan",
   },
   website: {
     title: "Website",
     subtitle: "allan-lago.herokuapp.com",
-    component: <AiOutlineGlobal className="clickable-image" />,
+    component: <IoIosGlobe className="clickable-image" />,
     href: "https://allan-lago.herokuapp.com/",
   },
   github: {
     title: "GitHub",
     subtitle: "/alago1",
-    component: <AiOutlineGithub className="clickable-image" />,
+    component: <IoLogoGithub className="clickable-image" />,
     href: "https://github.com/alago1",
   },
   linkedin: {
     title: "LinkedIn",
     subtitle: "in/allan-lago-a6b5131a8",
-    component: <TiSocialLinkedin className="clickable-image" />,
+    component: <IoLogoLinkedin className="clickable-image" />,
     href: "https://www.linkedin.com/in/allan-lago-a6b5131a8/",
   },
 };
